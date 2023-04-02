5bdc03d209ebad10b729c4292a4696019379a894
Artemis <Artemis@w1z0.xyz>
2023-04-02 13:43:22 +0100

    Feat: use async over fetch

--- background.js
+++ background.js
@@ -2,28 +2,29 @@ const appToken = '';
 const userKey = '';
 const binanceAddress = '';
 
-function sendPushoverNotification(message) {
-    fetch('https://api.pushover.net/1/messages.json', {
+async function sendPushoverNotification(message) {
+    try {
+        const response = await fetch('https://api.pushover.net/1/messages.json', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: `token=${encodeURIComponent(appToken)}&user=${encodeURIComponent(userKey)}&message=${encodeURIComponent(message)}`,
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Pushover notification sent:', data);
-        })
-        .catch((error) => {
-            console.error('Error sending Pushover notification:', error);
         });
+
+        const data = await response.json();
+        console.log('Pushover notification sent:', data);
+    } catch (error) {
+        console.error('Error sending Pushover notification:', error);
+    }
 }
 
 let intervalId;
 let checkDuration = 60 * 1000;
 
-function checkWallet() {
-    fetch("https://waxpeer.com/api/user", {
+async function checkWallet() {
+    try {
+        const response = await fetch("https://waxpeer.com/api/user", {
             headers: {
                 "accept": "application/json, text/plain, */*",
                 "accept-language": "en;q=0.6",
@@ -40,50 +41,55 @@ function checkWallet() {
             method: "GET",
             mode: "cors",
             credentials: "include"
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.success && data.user.wallet > 1000) {
-                fetch("https://waxpeer.com/api/create-withdraw", {
-                        "headers": {
-                            "accept": "application/json, text/plain, */*",
-                            "accept-language": "en;q=0.6",
-                            "cache-control": "no-cache",
-                            "content-type": "application/json",
-                            "pragma": "no-cache",
-                            "sec-fetch-dest": "empty",
-                            "sec-fetch-mode": "cors",
-                            "sec-fetch-site": "same-origin",
-                            "sec-gpc": "1"
-                        },
-                        "body": JSON.stringify({ "type": "usdt_prc20", "wallet": binanceAddress, "amount": data.user.wallet, "auto": true, "fa": null }),
-                        "method": "POST",
-                        "mode": "cors",
-                        "credentials": "include"
-                    })
-                    .then((response) => response.json())
-                    .then((withdrawData) => {
-                        if (withdrawData.success) {
-                            sendPushoverNotification(`Automated Wax Withdrawal amount: $${data.user.wallet / 1000}`);
-                            // reset check duration to 1 minute on success
-                            checkDuration = 60 * 1000;
-                        } else {
-                            checkDuration = 2 * checkDuration;
-                            sendPushoverNotification(`Withdrawal failed: ${withdrawData.msg}`);
-                        }
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                        // double check duration for each error, reduing the frequency of checks
-                        checkDuration = 2 * checkDuration;
-                    });
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            sendPushoverNotification(`Error: ${error.message}`);
         });
+
+        const data = await response.json();
+
+        if (data.success && data.user.wallet > 1000) {
+            const withdrawResponse = await fetch("https://waxpeer.com/api/create-withdraw", {
+                "headers": {
+                    "accept": "application/json, text/plain, */*",
+                    "accept-language": "en;q=0.6",
+                    "cache-control": "no-cache",
+                    "content-type": "application/json",
+                    "pragma": "no-cache",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "sec-gpc": "1"
+                },
+                "body": JSON.stringify({ "type": "usdt_prc20", "wallet": binanceAddress, "amount": data.user.wallet, "auto": true, "fa": null }),
+                "method": "POST",
+                "mode": "cors",
+                "credentials": "include"
+            });
+
+            const withdrawData = await withdrawResponse.json();
+
+            if (withdrawData.success) {
+                sendPushoverNotification(`Automated Wax Withdrawal amount: $${data.user.wallet / 1000}`);
+                // reset check duration to 1 minute on success
+                checkDuration = 60 * 1000;
+                updateInterval();
+            } else {
+                checkDuration = 2 * checkDuration;
+                updateInterval();
+                sendPushoverNotification(`Withdrawal failed: ${withdrawData.msg}`);
+            }
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        sendPushoverNotification(`Error: ${error.message}`);
+    }
 }
+
+function updateInterval() {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    intervalId = setInterval(checkWallet, checkDuration);
+}
+
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'start_checking') {
